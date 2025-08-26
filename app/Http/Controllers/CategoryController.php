99e29024830a7be11712ede7aa85749f2e0139ac
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2025-08-26 21:05:23 +0500

    Tovar category get

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Tovar;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function display_category_tovars(Category $category, Tovar $tovar)
+    {
+        # Display Category Tovars
+        $categories = Category::all();
+        $tovars = Tovar::where('category', $category->id)->get();
+
+        return view('display_category_tovars', compact('tovars', 'categories'));
+    }
 }
