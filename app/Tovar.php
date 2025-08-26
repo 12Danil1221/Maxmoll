20d6747fcfe8663b0dfe51b388c868e0ace62242
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2025-08-26 12:48:24 +0500

    Связь один ко многим для Товаров с Категориями

--- app/Tovar.php
+++ app/Tovar.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tovar extends Model
 {
+
+    public function category(): HasMany
+    {
+        # One and Many
+        return $this->hasMany(Category::class);
+    }
+
     protected $fillable=['name', 'category', 'slug', 'price', 'image', 'description'];
 }
