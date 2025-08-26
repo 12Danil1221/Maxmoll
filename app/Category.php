a5575650f905d6a763bcbaf5967bc69d0593b329
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2025-08-26 16:44:09 +0500

    Связь один ко многим для Категории с Товарами

--- app/Category.php
+++ app/Category.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
+    public function tovar(): BelongsTo
+    {
+        # One and Many
+        return $this->BelongsTo(Tovar::class);
+    }
+
     protected $fillable = ['name', 'slug'];
     
 }
