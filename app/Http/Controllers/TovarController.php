12d4c03656f4cb10e925b2de6c87744d1d6b77bf
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2025-08-26 21:03:27 +0500

    Select and create model Card

--- app/Http/Controllers/TovarController.php
+++ app/Http/Controllers/TovarController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Card;
+use App\Category;
 use App\Tovar;
 use Illuminate\Http\Request;
 
@@ -10,8 +12,10 @@ class TovarController extends Controller
     public function display_card()
     {
         # display card
+        $cards = Card::all();
+        $categories = Category::all();
 
-        return view('display_card');
+        return view('display_card', compact('cards', 'categories'));
     }
     public function add_card(Request $request)
     {
@@ -20,10 +24,27 @@ class TovarController extends Controller
     'name'=>'required', 'category'=>'required', 'slug'=>'required', 'price'=>'required', 'image'=>'required', 'description'=>'required'
         ]);
         
-         Tovar::create($request->all());
+         Card::create($request->all());
 
         session()->flash('success', 'Товар добавлен в корзину');
 
         return redirect()->route('display_card');
+    }
+        public function destroy_card(Card $card)
+    {
+        # delete card
+        
+         $card->delete();
+
+        session()->flash('danger', 'Товар убран из корзины');
+
+        return redirect('welcome');
+    }
+    public function display_detail_tovar(Tovar $tovar)
+    {
+        # Display Detail Tovar
+        $categories = Category::all();
+
+        return view('display_detail_tovar', compact('tovar', 'categories'));
     }
 }
